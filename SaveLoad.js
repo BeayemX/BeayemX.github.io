d85409963145ca13d532374df35872ec481517f3
Christian Winter <christian.winter1990@gmail.com>
2016-06-02 00:04:42 +0200

    export as svg possible

--- SaveLoad.js
+++ SaveLoad.js
@@ -287,9 +287,57 @@ function SaveToDisk()
     var name = prompt("Save as: ");
 
     if (name) {
-        var data = JSON.stringify(currentProject.currentFile.lines);
-        var blob = new Blob([data], { type: "text/plain;charset=utf-8" });
-        saveAs(blob, name + ".json");
+        SaveAsJSON(name);
+    }
+}
+
+function SaveAsJSON(name)
+{
+    let data = JSON.stringify(currentProject.currentFile.lines);
+    let blob = new Blob([data], { type: "text/plain;charset=utf-8" });
+    saveAs(blob, name + ".json");
+}
+
+function ExportAsSVG()
+{
+    var name = prompt("Save as: ");
+
+    if (name) {
+        let svgData = '<svg xmlns="http://www.w3.org/2000/svg" version="1.1">\n';
+        let factor = 10; // TODO maybe prompt for scale factor? or create own export-dialog-prompt-window?
+        let linesArray = currentProject.currentFile.lines;
+
+        let shiftX = 0;
+        let shiftY = 0;
+
+        for (let i = 0; i < linesArray.length; ++i)
+        {
+            shiftX = Math.min(shiftX, linesArray[i].start.x, linesArray[i].end.x);
+            shiftY = Math.min(shiftY, linesArray[i].start.y, linesArray[i].end.y);
+        }
+        shiftX = -shiftX;
+        shiftY = -shiftY;
+
+        svgData += '\t<factor value="' + factor + '"/>\n';
+        svgData += '\t<shift x="' + shiftX + '" y="' + shiftY + '"/>\n';
+
+	    for (let i=0; i<linesArray.length; ++i)    
+        {
+	        svgData += 
+                '\t<line ' + 
+                'x1="'+ (shiftX + linesArray[i].start.x) * factor + '" ' + 
+                'y1="' + (shiftY + linesArray[i].start.y) * factor + '" ' +
+                'x2="' + (shiftX + linesArray[i].end.x) * factor + '" ' +
+                'y2="' + (shiftY + linesArray[i].end.y) * factor + '" ' +
+                'stroke="black" ' +
+                'stroke-width="1"' +            
+                '/> \n'
+        }
+        svgData += '</svg>';
+
+        // var blob = new Blob([svgData], { type: "application/svg+xml" });
+        let blob = new Blob([svgData], { type: "text/plain;charset=utf-8" });
+        saveAs(blob, name + ".svg");
     }
 }
 
@@ -311,6 +359,7 @@ function LoadFromDisk()
 			    )
 		    );
         }
+
         SetCurrentFile(null);
         Redraw();
     }
