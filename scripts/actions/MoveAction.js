49ac053a834400f124fd413bed8f706bcf4fc273
Christian Winter <christian.winter1990@gmail.com>
2016-06-24 01:15:50 +0200

    using vector classes

--- scripts/actions/MoveAction.js
+++ scripts/actions/MoveAction.js
@@ -7,38 +7,12 @@
     }
 
     Do() {
-        let delta = {
-            x: this.current.x - this.start.x,
-            y: this.current.y - this.start.y
-        };
+        let delta = this.current.copy().SubtractVector(this.start);
         UTILITIES.movePointsBy(this.points, delta);
     }
 
     Undo() {
-        let delta = {
-            x: this.start.x - this.current.x,
-            y: this.start.y - this.current.y
-        };
+        let delta = this.start.copy().SubtractVector(this.current);
         UTILITIES.movePointsBy(this.points, delta);
     }
 }
-/*
-class MoveAction {
-    // TODO shouldn't need to save points. if selection is also an action
-    constructor(points, current, start) {
-        this.points = points;
-        this.current = current;
-        this.start = start;
-    }
-
-    Do() {
-        let delta = this.current.copy().Subtract(this.start);
-        UTILITIES.movePointsBy(this.points, delta);
-    }
-
-    Undo() {
-        let delta = this.start.copy().Subtract(this.current);
-        UTILITIES.movePointsBy(this.points, delta);
-    }
-}
-*/
