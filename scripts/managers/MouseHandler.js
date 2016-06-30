f587b9055008f0655a26efe733baf28739217d4c
Christian Winter <christian.winter1990@gmail.com>
2016-06-30 21:52:54 +0200

    selection improvements. double click --> grow selection. tripple click. select linked

--- scripts/managers/MouseHandler.js
+++ scripts/managers/MouseHandler.js
@@ -60,92 +60,113 @@ class MouseHandler {
     }
 
     MouseDown(e) {
-        if (e.button == 0) // LMB
-        {
-            if (LOGIC.currentState == StateEnum.IDLE) {
-                LOGIC.setState(StateEnum.DRAWING);
-                this.downPoint = currentPosition.copy();
-                this.cursorPositionChanged();
+        if (e.detail == 1) {
+            if (e.button == 0) // LMB
+            {
+                if (LOGIC.currentState == StateEnum.IDLE) {
+                    LOGIC.setState(StateEnum.DRAWING);
+                    this.downPoint = currentPosition.copy();
+                    this.cursorPositionChanged();
+                }
+                else if (LOGIC.currentState == StateEnum.GRABBING) {
+                    // TODO HACK FIXME simulate cancel grab to reset grabbed-delta, because when action is
+                    // added to history it will call Do()...
+                    let resetDelta = KEYBOARD_HANDLER.grabStartPosition.subtractVector(currentPosition);
+                    UTILITIES.movePointsBy(DATA_MANAGER.currentFile.getAllSelectedPoints(), resetDelta, true);
+                    DATA_MANAGER.currentFile.cleanUpFile();
+                    grabInitializedWithKeyboard = false;
+                    LOGIC.setState(StateEnum.IDLE);
+                    DRAW_MANAGER.redraw();
+                }
+                else if (LOGIC.currentState == StateEnum.BORDERSELECTION) {
+                    UTILITIES.startAreaSelection(true);
+                }
             }
-            else if (LOGIC.currentState == StateEnum.GRABBING) {
-                // TODO HACK FIXME simulate cancel grab to reset grabbed-delta, because when action is
-                // added to history it will call Do()...
-                let resetDelta = KEYBOARD_HANDLER.grabStartPosition.subtractVector(currentPosition);
-                UTILITIES.movePointsBy(DATA_MANAGER.currentFile.getAllSelectedPoints(), resetDelta, true);
-                DATA_MANAGER.currentFile.cleanUpFile();
-                grabInitializedWithKeyboard = false;
-                LOGIC.setState(StateEnum.IDLE);
-                DRAW_MANAGER.redraw();
-            }
-            else if (LOGIC.currentState == StateEnum.BORDERSELECTION) {
-                UTILITIES.startAreaSelection(true);
-            }
-        }
-        else if (e.button == 2) // RMB
-        {
-            if (LOGIC.currentState == StateEnum.IDLE) {
-                if (!e.shiftKey)
-                    DATA_MANAGER.currentFile.clearSelection();
+            else if (e.button == 2) // RMB
+            {
+                if (LOGIC.currentState == StateEnum.IDLE) {
+                    if (!e.shiftKey)
+                        DATA_MANAGER.currentFile.clearSelection();
 
-                let lines = DATA_MANAGER.currentFile.currentObject.lines;
-                let pointsToChangeSelection = [];
+                    let lines = DATA_MANAGER.currentFile.currentObject.lines;
+                    let pointsToChangeSelection = [];
 
-                // TODO weird number but should be a third?
-                let limit = 0.25;
+                    // TODO weird number but should be a third?
+                    let limit = 0.25;
 
-                for (let i = 0; i < lines.length; i++) {
-                    if (UTILITIES.distancePointToLine(selectionCursor, lines[i]) <= cursorRange) {
-                        // TODO PERFORMANCE
-                        let startDist = lines[i].start.subtractVector(selectionCursor).sqrMagnitude();
-                        let endDist = lines[i].end.subtractVector(selectionCursor).sqrMagnitude();
+                    for (let i = 0; i < lines.length; i++) {
+                        if (UTILITIES.distancePointToLine(selectionCursor, lines[i]) <= cursorRange) {
+                            // TODO PERFORMANCE
+                            let startDist = lines[i].start.subtractVector(selectionCursor).sqrMagnitude();
+                            let endDist = lines[i].end.subtractVector(selectionCursor).sqrMagnitude();
 
-                        if (startDist < endDist * limit) 
-                            pointsToChangeSelection.push(lines[i].start);
-                        else if (endDist < startDist * limit)
-                            pointsToChangeSelection.push(lines[i].end);
-                        else {
-                            pointsToChangeSelection.push(lines[i].start);
-                            pointsToChangeSelection.push(lines[i].end);
+                            if (startDist < endDist * limit)
+                                pointsToChangeSelection.push(lines[i].start);
+                            else if (endDist < startDist * limit)
+                                pointsToChangeSelection.push(lines[i].end);
+                            else {
+                                pointsToChangeSelection.push(lines[i].start);
+                                pointsToChangeSelection.push(lines[i].end);
+                            }
                         }
                     }
+
+                    UTILITIES.changeSelectionForPoints(pointsToChangeSelection);
+
+                    if (pointsToChangeSelection != null) {
+                        KEYBOARD_HANDLER.grabStartPosition = currentPosition.copy();
+
+                        LOGIC.setState(StateEnum.GRABBING);
+                        this.grabInitializedWithRMBDown = true;
+                    }
+                }
+                else if (LOGIC.currentState == StateEnum.BORDERSELECTION) {
+                    UTILITIES.endAreaSelection();
+                }
+                else if (LOGIC.currentState == StateEnum.DRAWING) {
+                    this.CancelLinePreview();
                 }
 
-                UTILITIES.changeSelectionForPoints(pointsToChangeSelection);
-
-                if (pointsToChangeSelection != null) {
-                    KEYBOARD_HANDLER.grabStartPosition = currentPosition.copy();
-
-                    LOGIC.setState(StateEnum.GRABBING);
-                    this.grabInitializedWithRMBDown = true;
+                DRAW_MANAGER.redraw();
+            }
+            else if (e.button == 1) // MMB
+            {
+                if (LOGIC.currentState == StateEnum.BORDERSELECTION) {
+                    UTILITIES.startAreaSelection(false);
                 }
-            }
-            else if (LOGIC.currentState == StateEnum.BORDERSELECTION) {
-                UTILITIES.endAreaSelection();
-            }
-            else if (LOGIC.currentState == StateEnum.DRAWING) {
-                this.CancelLinePreview();
-            }
-
-            DRAW_MANAGER.redraw();
-        }
-        else if (e.button == 1) // MMB
-        {
-            if (LOGIC.currentState == StateEnum.BORDERSELECTION) {
-                UTILITIES.startAreaSelection(false);
+                else {
+                    var screenPos = UTILITIES.getMousePos(e);
+                    this.isPanning = true;
+                }
             }
             else {
-                var screenPos = UTILITIES.getMousePos(e);
-                this.isPanning = true;
+                console.log("Mouse button: \n"
+                    + "button: " + e.button + "\n"
+                    + "ctrlKey: " + e.ctrlKey + "\n"
+                    + "altKey: " + e.altKey + "\n"
+                    + "shiftKey: " + e.shiftKey + "\n"
+                    );
             }
         }
-        else {
-            console.log("Mouse button: \n"
-			    + "button: " + e.button + "\n"
-			    + "ctrlKey: " + e.ctrlKey + "\n"
-			    + "altKey: " + e.altKey + "\n"
-			    + "shiftKey: " + e.shiftKey + "\n"
-			    );
+        else if (e.detail == 2) {
+            if (e.button == 0) { // LMB 
+
+            }
+            else if (e.button == 1) {// MMB
+
+            }
+            else if (e.button == 2) {// RMB
+                LINE_MANIPULATOR.growSelection(true);
+            }
+
         }
+        else if (e.detail == 3) {
+
+            if (e.button == 2) {// RMB
+                LINE_MANIPULATOR.selectLinked();
+            }
+        }
+
         e.preventDefault();
     }
 
@@ -214,7 +235,7 @@ class MouseHandler {
             if (e.deltaX < 0)
                 cursorRange += step;
             else if (e.deltaX > 0)
-                cursorRange = Math.max(cursorRange-step, 1);
+                cursorRange = Math.max(cursorRange - step, 1);
 
         } else {
             if (e.deltaY < 0) // upscroll
