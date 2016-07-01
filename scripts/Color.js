87285a48b11ac030267f11a5a5f959ccef9d36bc
Christian Winter <christian.winter1990@gmail.com>
2016-07-01 16:10:35 +0200

    color hex string conversion

--- scripts/Color.js
+++ scripts/Color.js
@@ -14,6 +14,25 @@
         return 'rgba(' + this.r + ", " + this.g + ", " + this.b + ", " + this.a + ")";
     }
 
+    toHexString()
+    {
+        let hexString = "#";
+
+        hexString += this.getTwoDigitHexForNumber(this.r);
+        hexString += this.getTwoDigitHexForNumber(this.g);
+        hexString += this.getTwoDigitHexForNumber(this.b);
+
+        return hexString;
+    }
+
+    getTwoDigitHexForNumber(n)
+    {
+        let hex = n.toString(16);
+        if (hex.length < 2)
+            hex = "0" + hex;
+        return hex;
+    }
+
     static rgbaToColor(rgba)
     {
         let c = new Color(0, 0, 0, 0);
@@ -25,6 +44,17 @@
         c.g = Number(rgbaArray[1]);
         c.b = Number(rgbaArray[2]);
         c.a = Number(rgbaArray[3]);
+        return c;
+    }
+
+    static hexToColor(hex)
+    {
+        hex = hex.replace('#', '');
+
+        let c = new Color(0, 0, 0, 1);
+        c.r = parseInt(hex.substring(0, 2), 16);
+        c.g = parseInt(hex.substring(2, 4), 16);
+        c.b = parseInt(hex.substring(4, 6), 16);
         return c;
     }
 }
